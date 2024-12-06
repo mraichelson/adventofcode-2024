f41230f008f9e0066ddb1d41eaa8a2b1e2abd8e2
Michael Raichelson <i.am@michaelraichelson.com>
2024-12-06 08:57:52 -0600

    add code for handling empty lines/carriage returns at end of input files in boilerplate

--- _boilerplate.ts
+++ _boilerplate.ts
@@ -1,7 +1,10 @@
-import fs from 'fs'
+import fs from "fs"
 
-const inputFileContent = fs.readFileSync('./input.txt', 'utf-8')
+const inputFileContent = fs.readFileSync("./input.txt", "utf-8")
 
 inputFileContent.split(/\r?\n/).map((line) => {
-  console.log('line:', line)
+	// Address trailing carriage return in input files.
+	if (line.length > 0) {
+		console.log(line)
+	}
 })
